85a4b0483159c88f5f23143a1990bce66539ec7f
Avalon Gagodo <hopegag@gmail.com>
2021-10-12 22:40:47 +0000

    Added Github Actions to build

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,8 +14,7 @@ class ExampleTest extends TestCase
      */
     public function test_example()
     {
-        $response = $this->get('/');
-
+        $response = $this->post('api/register');
         $response->assertStatus(200);
     }
 }
