41c1c8b380933e6353096eab7c3d2278f15c49a1
Avalon Gagodo <hopegag@gmail.com>
2021-10-21 15:45:39 +0000

    Updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\App;
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
